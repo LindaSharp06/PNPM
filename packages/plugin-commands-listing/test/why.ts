LindaSharp06/PNPM

commit df44172a0e53e2b782c3a534b00071c46fa90a83

fix(list): the default depth is 0

packages/plugin-commands-listing/src/list.ts
@@ -108,7 +108,7 @@ export type ListCommandOptions = Pick<Config,
   'optional' |
   'production' |
   'selectedProjectsGraph'
-> & {
+> & Partial<Pick<Config, 'cliOptions'>> & {
   alwaysPrintRootPackage?: boolean,
   depth?: number,
   lockfileDir?: string,
@@ -126,12 +126,14 @@ export function handler (
     devDependencies: opts.dev !== false,
     optionalDependencies: opts.optional !== false,
   }
+  const depth = opts.cliOptions?.['depth'] ?? 0
   if (opts.recursive && opts.selectedProjectsGraph) {
     const pkgs = Object.values(opts.selectedProjectsGraph).map((wsPkg) => wsPkg.package)
-    return listRecursive(pkgs, args, { ...opts, include })
+    return listRecursive(pkgs, args, { ...opts, depth, include })
   }
   return render([opts.dir], args, {
     ...opts,
+    depth,
     include,
     lockfileDir: opts.lockfileDir || opts.dir,
   })

packages/plugin-commands-listing/test/recursive.ts
@@ -114,7 +114,7 @@ test('recursive list with shared-workspace-lockfile', async (t) => {
   const output = await list.handler([], {
     ...DEFAULT_OPTS,
     allProjects,
-    depth: 2,
+    cliOptions: { depth: 2 },
     dir: process.cwd(),
     recursive: true,
     selectedProjectsGraph,

commit 90fbc8c308d4b55ec6660bafac187b206ca4bafb

fix(why): should have depth Infinity
Fixes a regression introduced by df44172a0e53e2b782c3a534b00071c46fa90a83

packages/plugin-commands-listing/src/list.ts
@@ -1,7 +1,6 @@
 import { docsUrl } from '@pnpm/cli-utils'
 import { FILTERING, OPTIONS, UNIVERSAL_OPTIONS } from '@pnpm/common-cli-options-help'
 import { Config, types as allTypes } from '@pnpm/config'
-import PnpmError from '@pnpm/error'
 import list, { forPackages as listForPackages } from '@pnpm/list'
 import { IncludedDependencies } from '@pnpm/types'
 import { oneLine } from 'common-tags'
@@ -135,7 +134,7 @@ export function handler (
     ...opts,
     depth,
     include,
-    lockfileDir: opts.lockfileDir || opts.dir,
+    lockfileDir: opts.lockfileDir ?? opts.dir,
   })
 }
 

packages/plugin-commands-listing/src/why.ts
@@ -92,5 +92,11 @@ export function handler (
   if (!args.length) {
     throw new PnpmError('MISSING_PACKAGE_NAME', '`pnpm why` requires the package name')
   }
-  return list(args, { ...opts, depth: Infinity })
+  return list(args, {
+    ...opts,
+    cliOptions: {
+      ...(opts.cliOptions ?? {}),
+      depth: Infinity,
+    },
+  })
 }

packages/plugin-commands-listing/test/index.ts
@@ -1,6 +1,5 @@
 ///<reference path="../../../typings/index.d.ts" />
 import { WANTED_LOCKFILE } from '@pnpm/constants'
-import PnpmError from '@pnpm/error'
 import { list, why } from '@pnpm/plugin-commands-listing'
 import prepare, { preparePackages } from '@pnpm/prepare'
 import { stripIndent } from 'common-tags'
@@ -10,7 +9,9 @@ import path = require('path')
 import stripAnsi = require('strip-ansi')
 import test = require('tape')
 import writeYamlFile = require('write-yaml-file')
+
 import './recursive'
+import './why'
 
 test('listing packages', async (t) => {
   prepare(t, {
@@ -204,22 +205,5 @@ test.skip('list on a project with skipped optional dependencies', async (t) => {
       └── not-compatible-with-any-os 1.0.0 skipped
     `)
   }
-  t.end()
-})
-
-test('`pnpm why` should fail if no package name was provided', async (t) => {
-  prepare(t)
-
-  let err!: PnpmError
-  try {
-    const output = await why.handler([], {
-      dir: process.cwd(),
-    })
-  } catch (_err) {
-    err = _err
-  }
-
-  t.equal(err.code, 'ERR_PNPM_MISSING_PACKAGE_NAME')
-  t.ok(err.message.includes('`pnpm why` requires the package name'))
   t.end()
 })

packages/plugin-commands-listing/test/why.ts
@@ -1,0 +1,55 @@
+import PnpmError from '@pnpm/error'
+import { why } from '@pnpm/plugin-commands-listing'
+import prepare from '@pnpm/prepare'
+import { REGISTRY_MOCK_PORT } from '@pnpm/registry-mock'
+import { stripIndent } from 'common-tags'
+import execa = require('execa')
+import stripAnsi = require('strip-ansi')
+import test = require('tape')
+
+test('`pnpm why` should fail if no package name was provided', async (t) => {
+  prepare(t)
+
+  let err!: PnpmError
+  try {
+    const output = await why.handler([], {
+      dir: process.cwd(),
+    })
+  } catch (_err) {
+    err = _err
+  }
+
+  t.equal(err.code, 'ERR_PNPM_MISSING_PACKAGE_NAME')
+  t.ok(err.message.includes('`pnpm why` requires the package name'))
+  t.end()
+})
+
+test('"why" should find non-direct dependency', async (t) => {
+  prepare(t, {
+    dependencies: {
+      'dep-of-pkg-with-1-dep': '100.0.0',
+      'pkg-with-1-dep': '100.0.0',
+    },
+  })
+
+  await execa('pnpm', ['install', '--registry', `http://localhost:${REGISTRY_MOCK_PORT}`])
+
+  const output = await why.handler(['dep-of-pkg-with-1-dep'], {
+    dev: false,
+    dir: process.cwd(),
+    optional: false,
+  })
+
+  t.equal(stripAnsi(output), stripIndent`
+    Legend: production dependency, optional only, dev only
+
+    project@0.0.0 ${process.cwd()}
+
+    dependencies:
+    dep-of-pkg-with-1-dep 100.0.0
+    pkg-with-1-dep 100.0.0
+    └── dep-of-pkg-with-1-dep 100.0.0
+  `, 'prints prod deps only')
+
+  t.end()
+})